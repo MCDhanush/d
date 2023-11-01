ab28aca6efabca02167e938ac1f4fe0cc03e741e
Dhanush MC <dhanushmc93@gmail.com>
2023-11-01 17:05:08 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // const express = require("express"); // "type": "commonjs"
 const express = require("express"); // "type": "module"
 const app = express();
-// import connectDB from "./db/connectDB.js";
+const connectDB = require("./db/connectDB.js");
 
 const PORT = 4000;
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
 
-// connectDB();
+connectDB();
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
